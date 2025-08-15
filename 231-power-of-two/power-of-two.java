fc92391877899a9b6eeca5dfad4bfc4e22185fb1
SaranyaKannan28 <saranyakannan1111@gmail.com>
2025-08-15 15:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (41.41%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n>0 && Integer.bitCount(n) == 1;
+        if (n>0 && Integer.bitCount(n)==1){
+            return true;
+        }
+        else {
+            return false;
+        }
         
     }
 }
