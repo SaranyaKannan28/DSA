db5b94730eb2440842b9657ef7308ee3b05e355f
SaranyaKannan28 <saranyakannan1111@gmail.com>
2025-08-27 21:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (42.18%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,21 +1,6 @@
 class Solution {
     public int addDigits(int num) {
-        List<Integer> ob = new ArrayList<>();
-        if(num<10) return num;
-        while(num>9){
-            ob.clear();
-            int sum=0;
-            while(num!=0){
-                int digit = num%10;
-                ob.add(digit);
-                num=num/10;
-            }
-            for(int i=0;i<ob.size();i++){
-                sum+=ob.get(i);
-            }
-            num=sum;
-        };
-        return num;
-        
+         if (num == 0) return 0;
+        return (int)(1 + (num - 1) % 9);
     }
 }
