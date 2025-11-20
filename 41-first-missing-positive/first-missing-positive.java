3f8c233d5d6e309957f3ae4d2e8401e40cc8f31c
SaranyaKannan28 <saranyakannan1111@gmail.com>
2025-11-20 10:35:43 +0530

    Time: 82 ms (6.20%) | Memory: 92 MB (5.00%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,22 +1,18 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        int n=nums.length;
-        List<Integer> list = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        for(int m:nums){
-            set.add(m);
+    public int firstMissingPositive(int[] arr) {
+        int n = arr.length;
+        Set<Integer> set =  new TreeSet<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+        
+        for(int i:arr){
+            set.add(i);
         }
+        
         for(int i=1;i<=n+1;i++){
             if(!set.contains(i)){
-                list.add(i);
+                return i;
             }
         }
-
-        int min=Integer.MAX_VALUE;
-        for(int k:list){
-             min = Math.min(k,min);
-        }
-
-        return min;
+        return -1;
     }
 }
